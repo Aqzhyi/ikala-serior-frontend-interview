d90d5ea1036d55b9e7db5c69c526687aea9bf11b
pleasurazy <pleasurazy@gmail.com>
2015-11-16 14:28:37 +0800

    rails first go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LIVEhouseRor::Application.config.secret_token = 'd7a4ca0bfeb7af1bfd182ff43711a9988830414ec1d8e0b2f578056e8c8646fdf9b8f88553bb4711fca4d3917668a8723310ed0fb5447815f474fec1f56bffcb'
