d4f7d0adf576da6de19e766a84e1ff1820fcf60d
pleasurazy <pleasurazy@gmail.com>
2015-11-17 14:02:09 +0800

    upload img

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -16,17 +16,7 @@ class ImagesController < ApplicationController
 
   def create
     respond_to do |format|
-      format.json {
-        render text: '
-          [
-            "https://placehold.it/200x200&text=imgA",
-            "https://placehold.it/200x200&text=imgB",
-            "https://placehold.it/200x200&text=imgC",
-            "https://placehold.it/200x200&text=imgD",
-            "https://placehold.it/200x200&text=uploaded"
-          ]
-        '
-      }
+      format.json { render :json => {"data": params[:data]} }
     end
   end
 end
--- src/home/index.js
+++ src/home/index.js
@@ -11,24 +11,45 @@ app.directive('homeApp', () => {
 })
 
 controller.$inject = [
-  'Images'
+  '$q',
+  'Images',
 ]
 
-function controller(Images) {
+function controller($q, Images) {
   let vm = this
 
+  vm.imagesUpload = []
   vm.imagesPreview = []
   vm.images = Images.query()
 
   vm.imagesSelected = ($files) => {
     vm.imagesPreview = []
+    vm.imagesUpload = []
     ;[].forEach.call($files, (file) => {
       if (file.dataURL.match(/image/)) {
         vm.imagesPreview.push(file.dataURL)
+        vm.imagesUpload.push(file.dataURL)
       }
       else {
         vm.imagesPreview.push(`https://placehold.it/400x400&text=${file.name}`)
       }
+    })
+  }
+
+  vm.upload = () => {
+    let uploadPromises = []
+
+    vm.imagesUpload.forEach((dataURL) => {
+      let img = new Images({data: dataURL})
+
+      let promise = img.$save().then(({data}) => vm.images.push(new Images({data})))
+
+      uploadPromises.push(promise)
+    })
+
+    $q.all(uploadPromises).then(() => {
+      vm.imagesUpload = []
+      vm.imagesPreview = []
     })
   }
 }
