757d085af0af312c6c170a74bf437e7eead841dd
pleasurazy <pleasurazy@gmail.com>
2015-11-17 12:43:50 +0800

    add helper for image

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,7 @@ import angular from 'angular'
 import app from './app'
 import 'bootstrap-webpack'
 import './factories/images'
+import './helper/image'
 import './home/index'
 
 angular.bootstrap(document, [app.name])
---
+++ src/helper/image.js
@@ -0,0 +1,30 @@
+import app from '../app'
+
+app.factory('image', [
+  '$q',
+  factory
+])
+
+function factory($q) {
+  let image = this
+
+  image.readAsDataURL = readAsDataURL
+
+  return image
+
+  ///// implementation
+  function readAsDataURL(file) {
+
+    return new $q((done, reject) => {
+
+      if (file instanceof File) {
+        var reader = new FileReader()
+        reader.onloadend = (loadEvent) => done(loadEvent.target.result)
+        reader.readAsDataURL(file)
+      }
+      else {
+        reject(new Error(`Expected image.readAsDataURL(arg1): arg1 to be File instance, but got ${file}`))
+      }
+    })
+  }
+}
