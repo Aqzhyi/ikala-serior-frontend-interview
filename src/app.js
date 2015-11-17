295684365adcb8960fcacf113ad7155baf2c8eb5
pleasurazy <pleasurazy@gmail.com>
2015-11-17 11:57:12 +0800

    add package ngResource and config with csrfToken etc

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 import angular from 'angular'
+import 'angular-resource'
 
-let app = angular.module('myApp', [])
+let app = angular.module('myApp', ['ngResource'])
+
+app.config([
+  '$httpProvider',
+  ($http) => {
+    let csrfToken = document.querySelector('meta[name=csrf-token]').content
+
+    let csrfMethods = ['get', 'post', 'put', 'patch', 'delete']
+    let requestedWithMethods = ['get', 'post', 'put', 'patch', 'delete']
+
+    for (let methodName of csrfMethods) {
+      $http.defaults.headers[methodName] = $http.defaults.headers[methodName] || {}
+      $http.defaults.headers[methodName]['X-CSRF-Token'] = csrfToken
+    }
+
+    for (let methodName of requestedWithMethods) {
+      $http.defaults.headers[methodName] = $http.defaults.headers[methodName] || {}
+      $http.defaults.headers[methodName]['X-Requested-With'] = 'XMLHttpRequest'
+    }
+  }
+])
 
 export default app
