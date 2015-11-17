cfdf66fafb19113545a3c2964da6c64a986b008c
pleasurazy <pleasurazy@gmail.com>
2015-11-17 11:51:16 +0800

    add bootstrap for webpack

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,6 @@
 import angular from 'angular'
 import app from './app'
+import 'bootstrap-webpack'
 import './home/index'
 
 angular.bootstrap(document, [app.name])
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
       { test: /\.css$/, loader: 'style!css' },
       { test: '\.jpg$', loader: 'file-loader' },
       { test: '\.png$', loader: 'url-loader?mimetype=image/png' },
+
+      // bootstrap-webpack
+      // the url-loader uses DataUrls.
+      // the file-loader emits files.
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   }
 }
