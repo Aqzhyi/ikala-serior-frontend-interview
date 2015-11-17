0a20a3093a1970d7675d5b0861b49e1cdf60a2ee
pleasurazy <pleasurazy@gmail.com>
2015-11-17 13:04:37 +0800

    select and preview files

--- src/home/index.js
+++ src/home/index.js
@@ -17,5 +17,18 @@ controller.$inject = [
 function controller(Images) {
   let vm = this
 
+  vm.imagesPreview = []
   vm.images = Images.query()
+
+  vm.imagesSelected = ($files) => {
+    vm.imagesPreview = []
+    ;[].forEach.call($files, (file) => {
+      if (file.dataURL.match(/image/)) {
+        vm.imagesPreview.push(file.dataURL)
+      }
+      else {
+        vm.imagesPreview.push(`https://placehold.it/400x400&text=${file.name}`)
+      }
+    })
+  }
 }
