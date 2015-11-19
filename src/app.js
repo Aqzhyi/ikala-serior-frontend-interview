f23f8a00399e6cd843bbb528483374c39e5f67b3
pleasurazy <pleasurazy@gmail.com>
2015-11-20 07:27:43 +0800

    refactor, should be independent

--- src/app.js
+++ src/app.js
@@ -3,24 +3,4 @@ import 'angular-resource'
 
 let app = angular.module('myApp', ['ngResource'])
 
-app.config([
-  '$httpProvider',
-  ($http) => {
-    let csrfToken = document.querySelector('meta[name=csrf-token]').content
-
-    let csrfMethods = ['get', 'post', 'put', 'patch', 'delete']
-    let requestedWithMethods = ['get', 'post', 'put', 'patch', 'delete']
-
-    for (let methodName of csrfMethods) {
-      $http.defaults.headers[methodName] = $http.defaults.headers[methodName] || {}
-      $http.defaults.headers[methodName]['X-CSRF-Token'] = csrfToken
-    }
-
-    for (let methodName of requestedWithMethods) {
-      $http.defaults.headers[methodName] = $http.defaults.headers[methodName] || {}
-      $http.defaults.headers[methodName]['X-Requested-With'] = 'XMLHttpRequest'
-    }
-  }
-])
-
 export default app
--- src/factories/images.js
+++ src/factories/images.js
@@ -2,14 +2,37 @@ import app from '../app'
 
 app.factory('Images', [
   '$resource',
+  '$http',
   factory
 ])
 
-function factory($resource) {
+function factory($resource, $http) {
+
+  let csrfToken = document.querySelector('meta[name=csrf-token]').content
+  let headers = angular.copy($http.defaults.headers)
+
+  let csrfMethods = ['get', 'post', 'put', 'patch', 'delete']
+  let requestedWithMethods = ['get', 'post', 'put', 'patch', 'delete']
+
+  for (let methodName of csrfMethods) {
+    headers[methodName] = headers[methodName] || {}
+    headers[methodName]['X-CSRF-Token'] = csrfToken
+  }
+
+  for (let methodName of requestedWithMethods) {
+    headers[methodName] = headers[methodName] || {}
+    headers[methodName]['X-Requested-With'] = 'XMLHttpRequest'
+  }
+
   return $resource('/images', {}, {
     query: {
       method: 'GET',
       isArray: true,
+      headers: headers.get,
+    },
+    save: {
+      method: 'POST',
+      headers: headers.post,
     }
   })
 }
