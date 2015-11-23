72cfc410aee18d485d988b8bcf04a3ede4028109
pleasurazy <pleasurazy@gmail.com>
2015-11-23 14:25:05 +0800

    refactor: more stable, independent. And reduce side-effect

--- src/directives/filesDrop/index.js
+++ src/directives/filesDrop/index.js
@@ -1,56 +1,73 @@
 import app from '../../app'
 
-app.directive('filesDrop', () => {
+app.directive('filesDrop', [
+  '$q',
+  '$parse',
+  'image',
+  directive
+])
+
+function directive($q, $parse, image) {
   return {
     restrict: 'A',
-    controllerAs: 'vm',
-    controller,
-    scope: {
-      filesDrop: '&'
-    },
-  }
-})
-
-controller.$inject = [
-  '$scope',
-  '$element',
-  '$q',
-  'image',
-]
-
-function controller($scope, $element, $q, image) {
-  $element.on('dragover.filesDrop', fileDragover)
-  $element.on('drop.filesDrop', fileDrop)
-
-  $scope.$on('destory', () => {
-    $element.off('dragover.filesDrop')
-    $element.off('drop.filesDrop')
-  })
-
-  function fileDrop(event) {
-    fileDragover(event)
-
-    let dt = event.originalEvent.dataTransfer
-    let files = dt && dt.files || null
-
-    let promises = [].reduce.call(files, (prev, file) => {
-      let promise = image.readAsDataURL(file).then((dataURL) => file.dataURL = dataURL)
-      prev.push(promise)
-      return prev
-    }, [])
-
-    $q.all(promises).then(() => $scope.filesDrop({$files: files}))
+    scope: false,
+    link,
   }
 
-  function fileDragover(event) {
-    event.stopPropagation()
-    event.preventDefault()
+  function link($scope, $element, $attrs) {
+    $element.addClass('filesDrop')
+    $element[0].addEventListener('dragleave', fileDragleave)
+    $element[0].addEventListener('dragover', fileDragover)
+    $element[0].addEventListener('drop', fileDrop)
 
-    if (event.type == 'dragover') {
-      $element.css('outline', '1px dashed #999')
+    $scope.$on('$destroy', () => {
+      $element[0].removeEventListener('dragleave', fileDragleave)
+      $element[0].removeEventListener('dragover', fileDragover)
+      $element[0].removeEventListener('drop', fileDrop)
+    })
+
+    function fileDrop(event) {
+      event.stopPropagation()
+      event.preventDefault()
+
+      let dt = event.dataTransfer
+      let files = dt && dt.files || null
+
+      if (!files) {
+        $parse($attrs.filesDrop)($scope, {
+          $files: [],
+          $event: event,
+        })
+
+        fileDragleave(event)
+
+        return
+      }
+
+      let promises = [].reduce.call(files, (prev, file) => {
+        let promise = image.readAsDataURL(file).then((dataURL) => file.dataURL = dataURL)
+        prev.push(promise)
+        return prev
+      }, [])
+
+      $q.all(promises).then(() => {
+        $parse($attrs.filesDrop)($scope, {
+          $files: files,
+          $event: event,
+        })
+
+        fileDragleave(event)
+      })
     }
-    else {
-      $element.css('outline', 'none')
+
+    function fileDragover(event) {
+      event.stopPropagation()
+      event.preventDefault()
+      $element.addClass('filesDrop--dragover')
+    }
+
+    function fileDragleave() {
+      $element.removeClass('filesDrop--dragover')
     }
   }
 }
