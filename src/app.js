23470e2e83d88cc293f684acc67e648f6a82b596
pleasurazy <pleasurazy@gmail.com>
2015-11-23 16:18:00 +0800

    separate directive into npm:angular-files-drop

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular'
+import 'angular-files-drop'
 import 'angular-resource'
 
-let app = angular.module('myApp', ['ngResource'])
+let app = angular.module('myApp', ['ngResource', 'angular-files-drop'])
 
 export default app
